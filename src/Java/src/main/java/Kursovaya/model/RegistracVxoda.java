b76126eed1f44bb254c4b12f85159656ef5dc1ff
Flexberry-man <mail@flexberry.net>
2023-12-17 09:33:35 +0000

    Flexberry code generation

---
+++ src/Java/src/main/java/Kursovaya/model/RegistracVxoda.java
@@ -0,0 +1,57 @@
+package Kursovaya.model;
+
+import org.eclipse.persistence.annotations.Convert;
+import org.eclipse.persistence.annotations.Converter;
+import Kursovaya.utils.UUIDConverter;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Entity implementation class for Entity: РегистрацВхода
+ */
+@Entity(name = "IISKursovayaРегистрацВхода")
+@Table(schema = "public", name = "РегистрацВхода")
+public class RegistracVxoda {
+
+    @Id
+    @Converter(converterClass = UUIDConverter.class, name = "primarykey")
+    @Convert("primarykey")
+    @Column(name = "primarykey", length = 16, unique = true, nullable = false)
+    private UUID primarykey;
+
+    @Column(name = "Дата")
+    private Date дата;
+
+    @OneToMany(mappedBy = "registracvxoda", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private List<VxodSotrudnika> vxodsotrudnikas;
+
+    @OneToMany(mappedBy = "registracvxoda", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private List<VxodGostya> vxodgostyas;
+
+
+    public RegistracVxoda() {
+        super();
+    }
+
+    public void setPrimarykey(UUID primarykey) {
+        this.primarykey = primarykey;
+    }
+
+    public UUID getPrimarykey() {
+        return primarykey;
+    }
+
+    public Date getДата() {
+      return дата;
+    }
+
+    public void setДата(Date дата) {
+      this.дата = дата;
+    }
+
+
+}
---
+++ src/ember-app/app/mixins/regenerated/models/i-i-s-kursovaya-вход-гостя.js
@@ -0,0 +1,41 @@
+import Mixin from '@ember/object/mixin';
+import $ from 'jquery';
+import DS from 'ember-data';
+import { validator } from 'ember-cp-validations';
+import { attr, belongsTo, hasMany } from 'ember-flexberry-data/utils/attributes';
+
+export let Model = Mixin.create({
+  гость: DS.belongsTo('i-i-s-kursovaya-гость', { inverse: null, async: false }),
+  регистрацВхода: DS.belongsTo('i-i-s-kursovaya-регистрац-входа', { inverse: 'входГостя', async: false })
+});
+
+export let ValidationRules = {
+  гость: {
+    descriptionKey: 'models.i-i-s-kursovaya-вход-гостя.validations.гость.__caption__',
+    validators: [
+      validator('ds-error'),
+      validator('presence', true),
+    ],
+  },
+  регистрацВхода: {
+    descriptionKey: 'models.i-i-s-kursovaya-вход-гостя.validations.регистрацВхода.__caption__',
+    validators: [
+      validator('ds-error'),
+      validator('presence', true),
+    ],
+  },
+};
+
+export let defineProjections = function (modelClass) {
+  modelClass.defineProjection('ВходГостяE', 'i-i-s-kursovaya-вход-гостя', {
+    гость: belongsTo('i-i-s-kursovaya-гость', 'Гость', {
+      фИО: attr('Гость', { index: 1, hidden: true }),
+      карта: belongsTo('i-i-s-kursovaya-карта', '', {
+        кодКарты: attr('Номер карты', { index: 2 }),
+        комната: belongsTo('i-i-s-kursovaya-комната', '', {
+          номерКомнаты: attr('Комната', { index: 3 })
+        }, { index: -1, hidden: true })
+      }, { index: -1, hidden: true })
+    }, { index: 0, displayMemberPath: 'фИО' })
+  });
+};
